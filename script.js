f4af5eaf73c05d4bc5de0f136e61122240efed67
pa-rozo-540 <104699965+pa-rozo-540@users.noreply.github.com>
2024-08-01 10:29:13 -0400

    commit

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 document.getElementById('send-btn).addEventListener('click', sendMessage);
+
+function sendMessage() {
+    const chatInput = document.getElementById('chat-input');
+    const chatBox = document.getElementById('chat-box');
+    const message = chatInput.value.trim();
+
+    if (message !== '') {
+        //create new message element
+        const messageElement = document.createElement('div');
+        messageElement.className = 'message user';
+        messageElement.textContent = message;
+
+        //append the message to chat box
+        chatBox.appenChild(messageElement);
+
+        //Scroll to the bottom of the chat box
+        chatBox.scrollTop = chatBox.scrollHeight;
+
+        //Clear the input field
+        chatInput.value = '';
+    }
+}
